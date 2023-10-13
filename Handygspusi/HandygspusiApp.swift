12f212f22b7ddd510f02f68e1bbe04d182fbfad2
Dominik Mack <dominik.mack@icloud.com>
2023-10-13 18:05:42 +0200

    Initial Commit

---
+++ Handygspusi/HandygspusiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandygspusiApp.swift
+//  Handygspusi
+//
+//  Created by Dominik Mack on 13.10.23.
+//
+
+import SwiftUI
+
+@main
+struct HandygspusiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
