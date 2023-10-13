144f7d7c79fc6d5f1c92a1621217adf2669a0ebd
Dominik Mack <dominik.mack@icloud.com>
2023-10-13 18:53:32 +0200

    Fixed build issues

--- Handygspusi/Models/Contract.swift
+++ Handygspusi/Models/Contract.swift
@@ -11,15 +11,30 @@ import SwiftData
 @Model
 class Contract {
     
-    @Attribute(.unique) var id: String
+    @Attribute(.unique) var id: UUID
     var contractRuntimeInMonths: Int
     var provider: String
-    var netz: String
+    var network: String
     var tariff: String
     var connectionFee: Double
     var monthlyFee: Double
     var oneTimeDeviceCosts: Double
     var cashback: Double
     var freeMonths: Int
+    var url: String?
+    
+    init(id: UUID, contractRuntimeInMonths: Int = 24, provider: String, network: String, tariff: String, connectionFee: Double, monthlyFee: Double, oneTimeDeviceCosts: Double, cashback: Double, freeMonths: Int, url: String? = nil) {
+        self.id = id
+        self.contractRuntimeInMonths = contractRuntimeInMonths
+        self.provider = provider
+        self.network = network
+        self.tariff = tariff
+        self.connectionFee = connectionFee
+        self.monthlyFee = monthlyFee
+        self.oneTimeDeviceCosts = oneTimeDeviceCosts
+        self.cashback = cashback
+        self.freeMonths = freeMonths
+        self.url = url
+    }
     
 }
